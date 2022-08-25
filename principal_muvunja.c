f81504229fd0198741d5b5574b562b0e1556eadf
Muvunja <nilsomuvunja@gmail.com>
2022-08-25 18:49:35 -0200

    Implementacaoda funcao menu

--- principal_muvunja.c
+++ principal_muvunja.c
@@ -1,12 +1,44 @@
 #include <stdio.h>
+#include <math.h>
+
+// DECLARACAO DAS FUNCOES
+
+void menu ();
 
 
 void main ()
 {
 
+printf ("--BEM VINDO AO SISTEMA DE GESTAO ACADEMICA--\n");
+
+// invocacao da funcao menu
+menu ();
+
+    
+}
+
+void menu (){
+
+int opcao;
+printf ("1- Estatistica de medias dos estudante de um curso\n");
+printf ("2- Estatistica de docentes de um curso\n");
+
+printf ("Escolha uma das opcoes acima\n");
+scanf ("%d",&opcao);
+
+switch (opcao)
+{
+case 1:
+  printf ("Escolheu o menu de estatistica de estudantes");
+  break;
 
 
+case 2:
+  printf("Escolheu o menu de estatistica dos docentes");
+default:
+printf ("Escolheu a opcao errada");
+  break;
+}
 
 
-  return 0;  
 }
