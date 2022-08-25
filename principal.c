d57b27c490082c30c4c555de58ddf24d6d8e7af7
carlitoschitsumba <84841976+carlitoschitsumba@users.noreply.github.com>
2022-08-25 18:43:36 +0200

    Função Menu
    implementei a função menu do software

--- principal.c
+++ principal.c
@@ -1,10 +1,40 @@
 #include <stdio.h>
 #include <math.h>
 
+//DECLARAÇÃO DAS FUNÇÕES
+void menu();
+
 void main (){
+printf("---BEM VINDO AO SISTEMA DE GESTÃO ACADÊMICA---\n");
+
+//invocar a função menu
+menu();
 
 
+}
 
 
+void menu(){
+int opcao;
+printf("1 - Estatisticas de medias de estudantes de um curso\n");
+printf("2 - Estatisticas de Docentes de um curso\n\n");
+
+printf("Escolha uma das opcoes acima\n");
+scanf("%d",&opcao);
+
+switch (opcao)
+{
+case 1:
+    printf("Escolheu o menu de medias de estudantes");
+    break;
+
+case 2:
+    printf("Escolheu o menu de estatisticas de docentes");
+    break;
+
+default:
+    printf("Opcao invalida por favor escolha entre 1 e 2");
+    break;
+}
 
 }
